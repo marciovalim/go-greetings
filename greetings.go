7dc9b4acfcada91ac02e82c2c7c0bbe9c8557286
marciovalim <marciogsvalim@gmail.com>
2022-08-16 07:43:59 -0300

    updated Hello

--- greetings.go
+++ greetings.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %v. Be very welcome!", name)
+	return fmt.Sprintf("Hello, %v. Be very much welcome!", name)
 }
